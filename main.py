48955c150fb53270b50025bfd508ab0834b93797
Jesus de Mula Cano <demula@gmail.com>
2012-08-23 22:45:18 +0200

    añadir toda la estructura completa de la web

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python2
-#
+# -*- coding: utf-8 -*-
 # Copyright 2007 Google Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -27,7 +27,7 @@ class MainHandler(webapp2.RequestHandler):
     def get(self):
         header_images = ["cabecera1w.jpg", "cabecera2w.jpg", "cabecera3w.jpg", "cabecera4w.jpg"]
         template_values = {
-		    'page_name': "Portada",
+            'page_name': "Portada",
             'site_name': "Club In-Line Sancti Petri",
             'site_address': "http://www.inlinesanctipetri.com",
             'header_image': random.choice(header_images)
@@ -35,16 +35,83 @@ class MainHandler(webapp2.RequestHandler):
         template = jinja_environment.get_template('index.html')
         self.response.out.write(template.render(template_values))
 
+class EscuelasHandler(webapp2.RequestHandler):
+    def get(self):
+        template_values = {
+            'page_name': "Escuelas",
+            'site_name': "Club In-Line Sancti Petri",
+            'site_address': "http://www.inlinesanctipetri.com"
+        }
+        template = jinja_environment.get_template('escuelas.html')
+        self.response.out.write(template.render(template_values))
+
+class SeniorHandler(webapp2.RequestHandler):
+    def get(self):
+        template_values = {
+            'page_name': "Categoria Senior",
+            'site_name': "Club In-Line Sancti Petri",
+            'site_address': "http://www.inlinesanctipetri.com"
+        }
+        template = jinja_environment.get_template('senior.html')
+        self.response.out.write(template.render(template_values))
+
+class CompeticionesHandler(webapp2.RequestHandler):
+    def get(self):
+        template_values = {
+            'page_name': "Competiciones",
+            'site_name': "Club In-Line Sancti Petri",
+            'site_address': "http://www.inlinesanctipetri.com"
+        }
+        template = jinja_environment.get_template('competiciones.html')
+        self.response.out.write(template.render(template_values))
+
+class QuienesHandler(webapp2.RequestHandler):
+    def get(self):
+        template_values = {
+            'page_name': "Quienes Somos",
+            'site_name': "Club In-Line Sancti Petri",
+            'site_address': "http://www.inlinesanctipetri.com"
+        }
+        template = jinja_environment.get_template('quienes_somos.html')
+        self.response.out.write(template.render(template_values))
+
+class ContactaHandler(webapp2.RequestHandler):
+    def get(self):
+        template_values = {
+            'page_name': "Contacta",
+            'site_name': "Club In-Line Sancti Petri",
+            'site_address': "http://www.inlinesanctipetri.com"
+        }
+        template = jinja_environment.get_template('contacta.html')
+        self.response.out.write(template.render(template_values))
+
 class ApuntateHandler(webapp2.RequestHandler):
     def get(self):
         template_values = {
+            'page_name': "Apúntate",
             'site_name': "Club In-Line Sancti Petri",
-            'site_address': "http://www.inlinesanctipetri.com",
+            'site_address': "http://www.inlinesanctipetri.com"
         }
-
         template = jinja_environment.get_template('apuntate.html')
         self.response.out.write(template.render(template_values))
 
+class SubscribirHandler(webapp2.RequestHandler):
+    def get(self):
+        template_values = {
+            'page_name': "Subscribir",
+            'site_name': "Club In-Line Sancti Petri",
+            'site_address': "http://www.inlinesanctipetri.com"
+        }
+        template = jinja_environment.get_template('subscribir.html')
+        self.response.out.write(template.render(template_values))
+
+
 app = webapp2.WSGIApplication([('/', MainHandler),
-                              ('/apuntate', ApuntateHandler)],
+                              ('/escuelas/', EscuelasHandler),
+                              ('/senior/', SeniorHandler),
+                              ('/competiciones/', CompeticionesHandler),
+                              ('/quienes-somos/', QuienesHandler),
+                              ('/contacta/', ContactaHandler),
+                              ('/apuntate/', ApuntateHandler),
+                              ('/subscribir/', SubscribirHandler)],
                               debug=True)
